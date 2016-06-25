bd1ea3d0c1453540eabe55821df46c4a3825f50f
Reid Paape <wrpaape@gmail.com>
2016-06-25 15:15:54 -0500

    removing count_string_alloc

--- src/mysql_seed_count_string.c
+++ src/mysql_seed_count_string.c
@@ -1,66 +1,26 @@
 #include "mysql_seed_count_string.h"
 
 
+extern inline unsigned int
+count_string_digit_count(const size_t n);
 
-extern inline struct CountString *
-count_string_create(const size_t upto);
+inline size_t
+count_string_char_count(const size_t upto);
+
+char *
+count_string_alloc(char *restrict *const restrict string,
+		   const size_t upto);
 
 extern inline void
-count_string_destroy(struct CountString *restrict string);
+log_csa_failure(const enum CountStringAllocStatus status,
+		const size_t upto);
+inline void
+count_string_log_alloc_failure(const size_t upto,
+			       const char *const restrict failure);
 
+extern inline char *
+count_string_put_digits(char *restrict ptr,
+			size_t upto);
 
-
-void
-log_csa_failure(const enum CountStringAllocateFlag status,
-		const size_t upto)
-{
-
-	seed_log_handle_lock();
-
-	seed_log_append_string("failed to allocate count string memory for "
-			       "'upto' of ");
-
-	seed_log_append_digits(upto);
-
-	seed_log_append_string(" ('UPTO_MAX' = " EXPAND_STRINGIFY(UPTO_MAX)
-			       ")\n"
-			       "reason:\n\t");
-	switch (status) {
-
-	case CSA_FAILURE_UPTO_MAX_EXCEEDED:
-		seed_log_append_string("'UPTO_MAX' exceeded\n\n");
-		break;
-
-	case CSA_FAILURE_OUT_OF_MEMORY:
-		seed_log_append_string("malloc failure (out of memory)\n\n");
-		break;
-
-	default:
-		seed_log_append_string("unknown\n\n");
-	}
-
-	seed_log_handle_unlock();
-}
-
-
-/* allocates space for count string 'string' */
-enum CountStringAllocateFlag
-count_string_allocate(struct CountString *restrict *const restrict string,
-		      const size_t upto)
-{
-	if (upto > UPTO_MAX)
-		return CSA_FAILURE_UPTO_MAX_EXCEEDED;
-
-	const size_t count_chars = sizeof(char) * count_string_char_count(upto);
-
-	*string = malloc(sizeof(struct CountString)
-			 + (sizeof(char) * count_chars));
-
-	if ((*string) == NULL)
-		return CSA_FAILURE_OUT_OF_MEMORY;
-
-	(*string)->digits      = (char *restrict) ((*string) + 1l);
-	(*string)->count_chars = count_chars;
-
-	return CSA_SUCCESS;
-}
+extern inline char *
+count_string_create(const size_t upto);
--- src/mysql_seed_count_string.h
+++ src/mysql_seed_count_string.h
@@ -10,10 +10,10 @@
 
 #if (SIZE_MAX < UINT32_MAX)
 #	define UPTO_MAX 9999lu
-#	define UPTO_MAX_DIGIT_COUNT 4lu
+#	define UPTO_MAX_DIGIT_COUNT 4u
 #else
 #	define UPTO_MAX 99999999lu
-#	define UPTO_MAX_DIGIT_COUNT 8lu
+#	define UPTO_MAX_DIGIT_COUNT 8u
 #endif /* if (SIZE_MAX < UINT32_MAX) */
 
 #define SIZE_1_9		18lu	   /* (1 digit  + '\0') * 9 */
@@ -35,22 +35,35 @@
 
 
 
-/* declarations
- *─────────────────────────────────────────────────────────────────────────── */
-enum CountStringAllocateFlag {
-	CSA_SUCCESS,
-	CSA_FAILURE_UPTO_MAX_EXCEEDED,
-	CSA_FAILURE_OUT_OF_MEMORY
-};
-
-struct CountString {
-	char *restrict digits;
-	size_t count_chars;
-};
 
 
 /* misc helper functions
  *─────────────────────────────────────────────────────────────────────────── */
+/* count of decimal digits of 'n' when '0 <= n <= UPTO_MAX' */
+inline unsigned int
+count_string_digit_count(const size_t n)
+{
+#if (UPTO_MAX == 99999999lu)
+	if (n < 10000lu) {
+#endif	/*  if (UPTO_MAX == 9999999lu) */
+		if (n < 100lu) {
+			return (n < 10lu)	? 1u : 2u;
+		} else {
+			return (n < 1000lu)	? 3u : 4u;
+		}
+#if (UPTO_MAX == 99999999lu)
+	} else {
+		if (n < 1000000lu) {
+			return (n < 100000lu)   ? 5u : 6u;
+		} else {
+			return (n < 10000000lu) ? 7u : 8u;
+		}
+	}
+#endif	/*  if (UPTO_MAX == 9999999lu) */
+}
+
+/* count of chars required for "1", "2", ... "upto" null-terminated ascii
+ * strings plus a final '\0' character to indicate end */
 inline size_t
 count_string_char_count(const size_t upto)
 {
@@ -82,43 +95,94 @@ count_string_char_count(const size_t upto)
 }
 
 
-void
-log_csa_failure(const enum CountStringAllocateFlag status,
-		const size_t upto);
 
-enum CountStringAllocateFlag
-count_string_allocate(struct CountString *restrict *const restrict string,
-		      const size_t upto);
+/* allocates space for ascending string of strings of ascii numbers, 'string' */
+inline enum CountStringAllocStatus
+count_string_alloc(char *restrict *const restrict string,
+		      const size_t upto)
+{
+	if (upto > UPTO_MAX)
+		return CSA_FAILURE_UPTO_MAX_EXCEEDED;
 
+
+	return ((*string) == NULL)
+	     ? CSA_FAILURE_OUT_OF_MEMORY
+	     : CSA_SUCCESS;
+}
+
+inline void
+count_string_log_alloc_failure(const size_t upto,
+			       const char *const restrict failure)
+{
+
+	seed_log_handle_lock();
+
+	seed_log_append_string("failed to allocate count string memory for "
+			       "'upto' of ");
+
+	seed_log_append_digits(upto);
+
+	seed_log_append_string(" ('UPTO_MAX' = " EXPAND_STRINGIFY(UPTO_MAX)
+			       ")\n"
+			       "reason:\n\t");
+
+	seed_log_append_string(failure);
+
+	seed_log_handle_unlock();
+}
+
+inline char *
+count_string_put_digits(char *restrict ptr,
+			size_t upto);
+{
+	*ptr = '\0';
+	--ptr;
+
+	switch (count_string_digit_count(upto)) {
+#if (UPTO_MAX_DIGIT_COUNT == 8u)
+	case 8u:
+	case 7u:
+	case 6u:
+	case 5u:
+#endif /* if (UPTO_MAX_DIGIT_COUNT == 8u) */
+	case 4u:
+	case 3u:
+	case 2u:
+	case 1u:
+	default:
+	}
+}
 
 
 /* top-level functions
  *─────────────────────────────────────────────────────────────────────────── */
-inline struct CountString *
+inline char *
 count_string_create(const size_t upto)
 {
+	if (upto > UPTO_MAX) {
+		count_string_log_alloc_failure("'UPTO_MAX' exceeded\n")
+		return NULL;
+	}
 
-	struct CountString *restrict string;
+	char *const restrict string = malloc(sizeof(char)
+					     * count_string_char_count(upto));
 
-	const enum CountStringAllocateFlag
-	csa_status = count_string_allocate(&string,
-					   upto);
 
+	if (string == NULL) {
+		count_string_log_alloc_failure("malloc failure (out of memory)\n")
+		return NULL;
+	}
+
+	const enum CountStringAllocStatus status = count_string_alloc(&string,
+								      upto);
 	if (csa_status != CSA_SUCCESS) {
-		log_csa_failure(csa_status,
+		log_csa_failure(status,
 				upto);
 		return NULL;
 	}
 
-	count_string_init_digits(string->digits);
-
-	return string;
-}
-
-inline void
-count_string_destroy(struct CountString *restrict string)
-{
-	free(string);
+	return count_string_put_digits(string,
+				       upto);
 }
 
 
