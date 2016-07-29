ab6e58bdca45bb3d3c6ce6dbd2bd5923667e5e59
Reid Paape <wrpaape@gmail.com>
2016-07-29 15:04:20 -0500

    unix time

--- src/time/time_utils.c
+++ src/time/time_utils.c
@@ -1,15 +1,29 @@
 #include "time/time_utils.h"
 
-extern inline void timespec_now(struct timespec *restrict time);
+/* seconds since 00:00:00, January 1st, 1970 */
+extern inline bool
+time_status(time_t *const restrict now);
+extern inline void
+time_muffle(time_t *const restrict now);
+extern inline bool
+time_report(time_t *const restrict now,
+	    const char *restrict *const restrict failure);
+extern inline void
+time_handle(time_t *const restrict now,
+	    Handler *const handle,
+	    void *arg);
+extern inline void
+time_handle_cl(time_t *const restrict now,
+	       const struct HandlerClosure *const restrict fail_cl);
 
+extern inline void
+timespec_now(struct timespec *restrict time);
 extern inline long
 timespec_diff_nano(const struct timespec *const restrict time0,
 		   const struct timespec *const restrict time1);
-
 extern inline void
 timespec_offset(struct timespec *const restrict time,
 		const struct timespec *const restrict offset);
-
 extern inline void
 timespec_offset_now(struct timespec *restrict time,
 		    const struct timespec *const restrict offset);
--- src/time/time_utils.h
+++ src/time/time_utils.h
@@ -5,13 +5,15 @@
 /* EXTERNAL DEPENDENCIES
  * ▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼ */
 
-#include <time.h>
-#include <sys/time.h>
+#include <time.h>		/* time */
+#include <sys/time.h>		/* timespec */
+#include "utils/fail_switch.h"	/* bool, error macros */
+#include "utils/closure.h"	/* Handler, HandlerClosure */
 
 #ifdef __MACH__
-#include <mach/clock.h>
-#include <mach/mach.h>
-#endif
+#	include <mach/clock.h>
+#	include <mach/mach.h>
+#endif /* ifdef __MACH__ */
 
 /* ▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲
  * EXTERNAL DEPENDENCIES
@@ -19,6 +21,16 @@
  *
  * TYPEDEFS, ENUM AND STRUCT DEFINITIONS
  * ▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼ */
+
+struct TimeStamp {
+	uint16_t years;
+	uint8_t months;
+	uint8_t days;
+	uint8_t hours;
+	uint8_t minutes;
+	uint8_t seconds;
+};
+
 /* ▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲
  * TYPEDEFS, ENUM AND STRUCT DEFINITIONS
  *
@@ -26,7 +38,18 @@
  * CONSTANTS
  * ▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼ */
 
+#define TIME_ERROR ((time_t) -1)
 #define ONE_BILLION 1000000000l
+
+#define SECONDS_PER_YEAR	((time_t) 3153600)	/* no leap seconds */
+#define SECONDS_PER_DAY		((time_t) 86400)
+#define SECONDS_PER_HOUR	((time_t) 3600)
+#define SECONDS_PER_MINUTE	((time_t) 60)
+
+/* error messages
+ * ────────────────────────────────────────────────────────────────────────── */
+#define TIME_FAILURE_MESSAGE						\
+FAILURE_REASON("time", "unknown")
 
 /* ▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲
  * CONSTANTS
@@ -40,6 +63,78 @@
  *
  * TOP-LEVEL FUNCTIONS
  * ▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼ */
+
+/* seconds since 00:00:00, January 1st, 1970 */
+inline bool
+time_status(time_t *const restrict now)
+{
+	return time(now) >= 0;
+}
+
+inline void
+time_muffle(time_t *const restrict now)
+{
+	(void) time(now);
+}
+
+#define FAIL_SWITCH_ROUTINE time
+#define FAIL_SWITCH_FAILURE_POINTER failure
+
+inline bool
+time_report(time_t *const restrict now,
+	    const char *restrict *const restrict failure)
+{
+	if (time(now) >= 0)
+		return true;
+
+	switch (errno) {
+	FAIL_SWITCH_ERRNO_CASE_1(ENOSYS,
+				 "function not implemented")
+	FAIL_SWITCH_ERRNO_DEFAULT_CASE()
+	}
+}
+
+inline void
+time_handle(time_t *const restrict now,
+	    Handler *const handle,
+	    void *arg)
+{
+	const char *restrict failure;
+
+	if (time_report(now,
+			&failure))
+		return;
+
+	handle(arg,
+	       failure);
+	__builtin_unreachable();
+}
+
+inline void
+time_handle_cl(time_t *const restrict now,
+	       const struct HandlerClosure *const restrict fail_cl)
+{
+	const char *restrict failure;
+
+	if (time_report(now,
+			&failure))
+		return;
+
+	handler_closure_call(fail_cl,
+			     failure);
+	__builtin_unreachable();
+}
+
+/* tm_sec + tm_min*60 + tm_hour*3600 + tm_yday*86400 + */
+/* (tm_year-70)*31536000 + ((tm_year-69)/4)*86400 - */
+/* ((tm_year-1)/100)*86400 + ((tm_year+299)/400)*86400 */
+inline void
+time_stamp_init(struct TimeStamp *const restrict time_stamp,
+		time_t time)
+{
+	time_stamp->years = 1970 + (time / SECONDS_PER_YEAR);
+	time %= SECONDS_PER_YEAR;
+}
 
 /* taken from stackoverflow user 'jbenet''s answer to
  * https://stackoverflow.com/questions/5167269/clock-gettime-alternative-in-mac-os-x
@@ -97,5 +192,8 @@ timespec_offset_now(struct timespec *restrict time,
 /* ▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲
  * HELPER FUNCTIONS */
 
+
+#undef FAIL_SWITCH_ROUTINE
+#undef FAIL_SWITCH_FAILURE_POINTER
 
 #endif /* ifndef MYSQL_SEED_TIME_TIME_UTILS_H_ */
