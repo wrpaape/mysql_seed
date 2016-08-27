6a1cbd903c404b7dfa7f0a0047bfe677b281b138
Reid Paape <wrpaape@gmail.com>
2016-08-27 18:44:53 -0500

    build_column_integer_random_upto_group working

--- src/generate/column_integer_random.c
+++ src/generate/column_integer_random.c
@@ -102,7 +102,6 @@ build_column_integer_random_group(void *arg)
 {
 	size_t rem_cells;
 	size_t rem_group;
-
 	char *restrict ptr;
 	char *restrict group_integer;
 	size_t *restrict group;
@@ -215,7 +214,6 @@ build_column_integer_random_group(void *arg)
 		}
 	}
 
-
 	/* increment table length */
 	length_lock_increment(&table->total,
 			      ptr - column->rowspans_from->cell,
@@ -302,7 +300,6 @@ build_column_integer_random_from_group(void *arg)
 {
 	size_t rem_cells;
 	size_t rem_group;
-
 	char *restrict ptr;
 	char *restrict group_integer;
 	size_t *restrict group;
@@ -416,7 +413,6 @@ build_column_integer_random_from_group(void *arg)
 		}
 	}
 
-
 	/* increment table length */
 	length_lock_increment(&table->total,
 			      ptr - column->rowspans_from->cell,
@@ -501,6 +497,127 @@ build_column_integer_random_upto(void *arg)
 void
 build_column_integer_random_upto_group(void *arg)
 {
+	size_t rem_cells;
+	size_t rem_group;
+	char *restrict ptr;
+	char *restrict group_integer;
+	size_t *restrict group;
+	PutStringWidth *put_group_integer;
+
+	struct Column *const restrict column
+	= (struct Column *const restrict) arg;
+
+	const struct IntegerRandSpec *const restrict rand_spec
+	= &column->spec->type_q.integer.rand_spec;
+
+	const struct BoundIGeneratorClosure *const restrict upto_cl
+	= &rand_spec->gen.upto;
+
+	struct Table *const restrict table
+	= column->parent;
+
+	const struct Rowspan *const restrict until = table->rowspans_until;
+
+	const unsigned int col_count = table->col_count;
+
+	const size_t row_count = table->spec->row_count;
+
+	const size_t group_count = column->spec->grp_spec.count;
+
+	GroupPartitioner *const partition_groups
+	= column->spec->grp_spec.partition;
+
+	const size_t size_est = (sizeof(size_t) * group_count)
+			      + (rand_spec->width_max * row_count);
+
+	thread_try_catch_open(&free_nullify_cleanup,
+			      &column->contents);
+
+	column->contents = malloc(size_est);
+
+	if (column->contents == NULL) {
+		handler_closure_call(&column->fail_cl,
+				     BCIRF_GROUP_MALLOC_FAILURE);
+		__builtin_unreachable();
+	}
+
+	struct Rowspan *restrict from = column->rowspans_from;
+
+	group = (size_t *restrict) column->contents;
+
+	group_integer = partition_groups(group,
+					 group_count,
+					 row_count);
+
+	ptr = put_int(group_integer,
+		      upto_cl->generate(&upto_cl->params));
+	*ptr = '\0';
+	++ptr;
+
+	put_group_integer = PUT_STRING_WIDTH_MAP[ptr - group_integer];
+
+	rem_group = *group;
+
+	rem_cells = from->parent->row_count;
+
+	from->cell = ptr;
+
+	while (1) {
+		if (rem_cells > rem_group) {
+			rem_cells -= (rem_group + 1lu);
+
+			while (rem_group > 0lu) {
+				ptr = put_group_integer(ptr,
+							group_integer);
+				--rem_group;
+			}
+
+			group_integer = ptr;
+
+			ptr = put_int(group_integer,
+				      upto_cl->generate(&upto_cl->params));
+			*ptr = '\0';
+			++ptr;
+
+			put_group_integer
+			= PUT_STRING_WIDTH_MAP[ptr - group_integer];
+
+			++group;
+
+			rem_group = *group - 1lu;
+
+		} else {
+			rem_group -= rem_cells;
+
+			while (rem_cells > 0lu) {
+				ptr = put_group_integer(ptr,
+							group_integer);
+				--rem_cells;
+			}
+
+			/* increment row_block length */
+			length_lock_increment(&from->parent->total,
+					      ptr - from->cell,
+					      &column->fail_cl);
+
+			/* skip to rowspan in next row */
+			from += col_count;
+
+			if (from >= until)
+				break;
+
+			from->cell = ptr;
+
+			rem_cells = from->parent->row_count;
+		}
+	}
+
+	/* increment table length */
+	length_lock_increment(&table->total,
+			      ptr - column->rowspans_from->cell,
+			      &column->fail_cl);
+
+	thread_try_catch_close();
 }
 
 void
