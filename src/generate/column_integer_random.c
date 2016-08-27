f5d2bc71738fec6b66139d4002b49e76bd818ba4
Reid Paape <wrpaape@gmail.com>
2016-08-27 16:46:26 -0500

    build_column_integer_random_range working

--- src/generate/column_integer_random.c
+++ src/generate/column_integer_random.c
@@ -255,6 +255,74 @@ build_column_integer_random_upto_group(void *arg)
 void
 build_column_integer_random_range(void *arg)
 {
+	struct Column *const restrict column
+	= (struct Column *const restrict) arg;
+
+	const struct IntegerRandSpec *const restrict rand_spec
+	= &column->spec->type_q.integer.rand_spec;
+
+	const struct BoundOffsetIGeneratorClosure *const restrict range_cl
+	= &rand_spec->gen.range;
+
+	struct Table *const restrict table
+	= column->parent;
+
+	const struct Rowspan *const restrict until = table->rowspans_until;
+
+	const unsigned int col_count = table->col_count;
+
+	const size_t row_count = table->spec->row_count;
+
+	const size_t size_est = rand_spec->width_max * row_count;
+
+	thread_try_catch_open(&free_nullify_cleanup,
+			      &column->contents);
+
+	column->contents = malloc(size_est);
+
+	if (column->contents == NULL) {
+		handler_closure_call(&column->fail_cl,
+				     BCIRU_MALLOC_FAILURE);
+		__builtin_unreachable();
+	}
+
+	struct Rowspan *restrict from = column->rowspans_from;
+
+	char *restrict ptr = column->contents;
+
+	size_t rem_cells;
+
+	do {
+		from->cell = ptr;
+
+		rem_cells = from->parent->row_count;
+
+		do {
+			ptr = put_int(ptr,
+				      range_cl->generate(&range_cl->params));
+
+			*ptr = '\0';
+			++ptr;
+
+			--rem_cells;
+
+		} while (rem_cells > 0lu);
+
+		/* add length of rowspan to row_block total */
+		length_lock_increment(&from->parent->total,
+				      ptr - from->cell,
+				      &column->fail_cl);
+
+		/* skip to rowspan in next row */
+		from += col_count;
+	} while (from < until);
+
+	/* increment table length */
+	length_lock_increment(&table->total,
+			      ptr - column->rowspans_from->cell,
+			      &column->fail_cl);
+
+	thread_try_catch_close();
 }
 
 void
--- src/mysql_seed_generate.h
+++ src/mysql_seed_generate.h
@@ -314,8 +314,8 @@ PARSE_ERROR_HEADER("no MAX_<TYPE> value provided for RAND_SPEC 'range'"	\
 		   " component, ignoring DB_SPEC starting with")
 
 #define ERROR_RANDOM_RANGE_MAX_LT_MIN					\
-PARSE_ERROR_HEADER("MAX_<TYPE> is less than MIN_<TYPE> for RAND_SPEC '"	\
-		   "'range' component, ignoring DB_SPEC starting with")
+"\n" ERROR_WRAP("MAX_<TYPE> is less than MIN_<TYPE> for RAND_SPEC '"	\
+		"range' component, ignoring DB_SPEC starting with") "\n"
 
 #define ERROR_INVALID_MIN_INTEGER_HEADER				\
 PARSE_ERROR_HEADER("invalid MIN_INT")
