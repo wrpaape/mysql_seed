e26ed9e50c8191ca0169436877341b83675eac74
Reid Paape <wrpaape@gmail.com>
2016-09-04 13:33:50 -0500

    replacing static inline functions with extern in mysql_seed

--- src/mysql_seed.c
+++ src/mysql_seed.c
@@ -1,5 +1,23 @@
 #include "mysql_seed.h"
 
+/* print error messsage
+ *─────────────────────────────────────────────────────────────────────────── */
+extern inline void
+print_no_mode_flag(void);
+
+extern inline void
+print_invalid_mode_flag(char *const restrict arg);
+
+
+/* parse stdin args
+ *─────────────────────────────────────────────────────────────────────────── */
+extern inline int
+mode_dispatch(char *const *const restrict from,
+	      const int rem_argc);
+
+
+/* main entry point
+ *─────────────────────────────────────────────────────────────────────────── */
 int
 main(int argc,
      char **argv)
@@ -11,117 +29,4 @@ main(int argc,
 
 	return mode_dispatch(argv + 1l,
 			     argc - 2);
-}
-
-/* parse stdin args
- *─────────────────────────────────────────────────────────────────────────── */
-inline int
-mode_dispatch(char *const *const restrict from,
-	      const int rem_argc)
-{
-	char *const restrict flag = *from;
-
-	if (*flag != '-')
-		goto INVALID_MODE_FLAG;
-
-	const char *const restrict arg = flag + 1l;
-	const char *const restrict rem = arg + 1l;
-
-	/* parse short mode flag */
-	switch (*arg) {
-	case '-':
-		break;	/* parse long mode flag */
-
-	case 'g':
-		if (LIKELY(*rem == '\0'))
-			return generate_dispatch(from + 1l,
-						 rem_argc);
-		goto INVALID_MODE_FLAG;
-
-
-	case 'h':
-		if (LIKELY(*rem == '\0'))
-			return help_dispatch(from + 1l,
-					     rem_argc);
-		goto INVALID_MODE_FLAG;
-
-
-	case 'e':
-		if (LIKELY(*rem == '\0'))
-			return execute_dispatch(from + 1l,
-						rem_argc);
-		goto INVALID_MODE_FLAG;
-
-	case 'r':
-		if (LIKELY(*rem == '\0'))
-			return remove_dispatch(from + 1l);
-
-	default:
-		goto INVALID_MODE_FLAG;
-	}
-
-	/* parse long mode flag */
-	switch (*rem) {
-	case 'g':
-		if (LIKELY(strings_equal("enerate", rem + 1l)))
-			return generate_dispatch(from + 1l,
-						 rem_argc);
-		break;
-
-	case 'h':
-		if (LIKELY(strings_equal("elp", rem + 1l)))
-			return help_dispatch(from + 1l,
-					     rem_argc);
-		break;
-
-	case 'e':
-		if (LIKELY(strings_equal("xecute", rem + 1l)))
-			return execute_dispatch(from + 1l,
-						rem_argc);
-		break;
-
-	case 'r':
-		if (LIKELY(strings_equal("emove", rem + 1l)))
-			return remove_dispatch(from + 1l);
-
-	default:
-		break;
-	}
-
-INVALID_MODE_FLAG:
-	print_invalid_mode_flag(flag);
-	return EXIT_FAILURE;
-}
-
-/* print error messsage and return 'EXIT_FAILURE'
- *─────────────────────────────────────────────────────────────────────────── */
-inline void
-print_no_mode_flag(void)
-{
-	write_muffle(STDERR_FILENO,
-		     FAILURE_NO_MODE_FLAG,
-		     sizeof(FAILURE_NO_MODE_FLAG) - 1lu);
-}
-
-inline void
-print_invalid_mode_flag(char *const restrict arg)
-{
-	static char buffer[ARG_INSPECT_BUFFER_SIZE] = {
-		FAILURE_INVALID_MODE_FLAG_HEADER
-	};
-
-	char *restrict
-	ptr = put_string_inspect(&buffer[0]
-				 + sizeof(FAILURE_INVALID_MODE_FLAG_HEADER)
-				 - 1lu,
-				 arg,
-				 LENGTH_INSPECT_MAX);
-
-	ptr = put_string_size(ptr,
-			      MORE_INFO_MESSAGE,
-			      sizeof(MORE_INFO_MESSAGE) - 1lu);
-
-	write_muffle(STDERR_FILENO,
-		     &buffer[0],
-		     ptr - &buffer[0]);
 }
--- src/mysql_seed.h
+++ src/mysql_seed.h
@@ -16,17 +16,116 @@ PARSE_FAILURE_MESSAGE("MODE not specified") MORE_INFO_MESSAGE
 #define FAILURE_INVALID_MODE_FLAG_HEADER				\
 PARSE_FAILURE_HEADER("invalid MODE flag")
 
-static inline int
-mode_dispatch(char *const *const restrict from,
-	      const int rem_argc);
-
-
-/* print error messsage and return 'EXIT_FAILURE'
+/* print error messsage
  *─────────────────────────────────────────────────────────────────────────── */
-extern inline void
-print_no_mode_flag(void);
+inline void
+print_no_mode_flag(void)
+{
+	write_muffle(STDERR_FILENO,
+		     FAILURE_NO_MODE_FLAG,
+		     sizeof(FAILURE_NO_MODE_FLAG) - 1lu);
+}
 
-static inline void
-print_invalid_mode_flag(char *const restrict arg);
+inline void
+print_invalid_mode_flag(char *const restrict arg)
+{
+	char buffer[ARG_INSPECT_BUFFER_SIZE];
 
+	char *restrict ptr
+	= put_string_size(&buffer[0],
+			  FAILURE_INVALID_MODE_FLAG_HEADER,
+			  sizeof(FAILURE_INVALID_MODE_FLAG_HEADER) - 1lu);
+
+	ptr = put_string_inspect(ptr,
+				 arg,
+				 LENGTH_INSPECT_MAX);
+
+	ptr = put_string_size(ptr,
+			      MORE_INFO_MESSAGE,
+			      sizeof(MORE_INFO_MESSAGE) - 1lu);
+
+	write_muffle(STDERR_FILENO,
+		     &buffer[0],
+		     ptr - &buffer[0]);
+}
+
+/* parse stdin args
+ *─────────────────────────────────────────────────────────────────────────── */
+inline int
+mode_dispatch(char *const *const restrict from,
+	      const int rem_argc)
+{
+	char *const restrict flag = *from;
+
+	if (*flag != '-')
+		goto INVALID_MODE_FLAG;
+
+	const char *const restrict arg = flag + 1l;
+	const char *const restrict rem = arg + 1l;
+
+	/* parse short mode flag */
+	switch (*arg) {
+	case '-':
+		break;	/* parse long mode flag */
+
+	case 'g':
+		if (LIKELY(*rem == '\0'))
+			return generate_dispatch(from + 1l,
+						 rem_argc);
+		goto INVALID_MODE_FLAG;
+
+
+	case 'h':
+		if (LIKELY(*rem == '\0'))
+			return help_dispatch(from + 1l,
+					     rem_argc);
+		goto INVALID_MODE_FLAG;
+
+
+	case 'e':
+		if (LIKELY(*rem == '\0'))
+			return execute_dispatch(from + 1l,
+						rem_argc);
+		goto INVALID_MODE_FLAG;
+
+	case 'r':
+		if (LIKELY(*rem == '\0'))
+			return remove_dispatch(from + 1l);
+
+	default:
+		goto INVALID_MODE_FLAG;
+	}
+
+	/* parse long mode flag */
+	switch (*rem) {
+	case 'g':
+		if (LIKELY(strings_equal("enerate", rem + 1l)))
+			return generate_dispatch(from + 1l,
+						 rem_argc);
+		break;
+
+	case 'h':
+		if (LIKELY(strings_equal("elp", rem + 1l)))
+			return help_dispatch(from + 1l,
+					     rem_argc);
+		break;
+
+	case 'e':
+		if (LIKELY(strings_equal("xecute", rem + 1l)))
+			return execute_dispatch(from + 1l,
+						rem_argc);
+		break;
+
+	case 'r':
+		if (LIKELY(strings_equal("emove", rem + 1l)))
+			return remove_dispatch(from + 1l);
+
+	default:
+		break;
+	}
+
+INVALID_MODE_FLAG:
+	print_invalid_mode_flag(flag);
+	return EXIT_FAILURE;
+}
 #endif	/* MYSQL_SEED_MYSQL_SEED_H_ */
